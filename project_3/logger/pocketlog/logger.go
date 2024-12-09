5e04164bce6fd946d9c4e8b2e1e61bc5dc907317
Seren4 <25780127+Seren4@users.noreply.github.com>
2024-12-09 15:24:19 +0100

    create signatures and New function

--- project_3/logger/pocketlog/logger.go
+++ project_3/logger/pocketlog/logger.go
@@ -1,6 +1,25 @@
 package pocketlog
 
-
+// Logger is used to log information
 type Logger struct{
+	threshold Level
+}
 
+// New returns you a logger; ready to log at hte required threshold
+func New(threshold Level) *Logger{
+	return &Logger{
+		threshold: threshold,
+	}
+}
+
+// Debugf formats and prints a message if the log level is debug or higher
+func (l *Logger) Debugf(format string, args ...any){
+}
+
+// Infof formats and prints a message if the log level is info or higher
+func (l *Logger) Infof(format string, args ...any){
+}
+
+// Errorf formats and prints a message if the log level is error
+func (l *Logger) Errorf(format string, args ...any){
 }
