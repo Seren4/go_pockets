64ffa5ebe6b8f689793be19e9a2eeccd99ee2488
Seren4 <25780127+Seren4@users.noreply.github.com>
2024-12-09 16:14:46 +0100

    create main, write Debugf

--- project_3/logger/pocketlog/logger.go
+++ project_3/logger/pocketlog/logger.go
@@ -16,7 +16,11 @@ func New(threshold Level) *Logger{
 
 // Debugf formats and prints a message if the log level is debug or higher
 func (l *Logger) Debugf(format string, args ...any){
-	
+	if l.threshold > LevelDebug {
+		return
+	}
+
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 // Infof formats and prints a message if the log level is info or higher
