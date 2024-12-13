05a8b4c3e54ea18081f1615eac52a7079db8c2fd
Seren4 <25780127+Seren4@users.noreply.github.com>
2024-12-13 13:45:57 +0100

    add output

--- project_3/logger/pocketlog/logger.go
+++ project_3/logger/pocketlog/logger.go
@@ -3,19 +3,19 @@ package pocketlog
 import "fmt"
 
 // Logger is used to log information
-type Logger struct{
+type Logger struct {
 	threshold Level
 }
 
 // New returns you a logger ready to log at required threshold
-func New(threshold Level) *Logger{
+func New(threshold Level) *Logger {
 	return &Logger{
 		threshold: threshold,
 	}
 }
 
 // Debugf formats and prints a message if the log level is debug or higher
-func (l *Logger) Debugf(format string, args ...any){
+func (l *Logger) Debugf(format string, args ...any) {
 	if l.threshold > LevelDebug {
 		return
 	}
@@ -24,13 +24,28 @@ func (l *Logger) Debugf(format string, args ...any){
 }
 
 // Infof formats and prints a message if the log level is info or higher
-func (l *Logger) Infof(format string, args ...any){
+func (l *Logger) Infof(format string, args ...any) {
+	if l.threshold > LevelDebug {
+		return
+	}
+
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 // Infof formats and prints a message if the log level is warn or higher
-func (l *Logger) Warnf(format string, args ...any){
+func (l *Logger) Warnf(format string, args ...any) {
+	if l.threshold > LevelDebug {
+		return
+	}
+
+	_, _ = fmt.Printf(format+"\n", args...)
 }
 
 // Errorf formats and prints a message if the log level is error
-func (l *Logger) Errorf(format string, args ...any){
+func (l *Logger) Errorf(format string, args ...any) {
+	if l.threshold > LevelDebug {
+		return
+	}
+
+	_, _ = fmt.Printf(format+"\n", args...)
 }
