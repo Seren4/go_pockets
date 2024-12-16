971e02774126da812ddc9fdf66d4014d79f8956f
Seren4 <25780127+Seren4@users.noreply.github.com>
2024-12-16 15:40:02 +0100

    pass logLevel after

--- project_3/logger/main.go
+++ project_3/logger/main.go
@@ -11,5 +11,8 @@ func main() {
 	lgr.Debugf("Hello, %s!", "world debug")
 	lgr.Infof("Hello, %s!", "world info")
 	lgr.Errorf("Hello error, %d %v", 2024, time.Now())
+	lgr.Logf(pocketlog.LevelError, "This will be logged, %d %v", 2024, time.Now())
+	lgr.Logf(pocketlog.LevelDebug, "This won't be logged, %d %v", 2024, time.Now())
+
 
 }
