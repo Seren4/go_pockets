5324f84615a9cb5a332fb80883f4c4feed793e08
Seren4 <25780127+Seren4@users.noreply.github.com>
2024-12-16 16:58:26 +0100

    test logger length main

--- project_3/logger/pocketlog/logger_test.go
+++ project_3/logger/pocketlog/logger_test.go
@@ -11,6 +11,12 @@ func ExampleLogger_Debugf() {
 	// Output: [DEBUG] Hello, world!
 }
 
+func ExampleLogger_Infof()() {
+	 debugLogger := pocketlog.New(pocketlog.LevelInfo, 6)
+	 debugLogger.Infof("Hello, %s!", "world")
+	// Output: [INFO] Hello,
+}
+
 // testWriter is a struct that implements io.Writer.
 // We use it to validate that we can write to a specific output.
 type testWriter struct {
@@ -61,12 +67,50 @@ func TestLogger_DebugfInfofErrorf(t *testing.T) {
 			//testedLogger.Infof(infoMsg)
 			// OR
 			testedLogger.Logf(pocketlog.LevelInfo, infoMsg)
+
 			//testedLogger.Errorf(errorMsg)
 			// OR
 			testedLogger.Logf(pocketlog.LevelError, errorMsg)
 
 			// TODO test loggerLength
 
+			if tw.contents != tc.expected {
+				t.Errorf("invalid contents, expected %q got %q", tc.expected, tw.contents)
+			}
+
+		})
+	}
+}
+
+func TestLoggerLength_DebugfInfofErrorf(t *testing.T) {
+	type TestCase struct {
+		level    pocketlog.Level
+		expected string
+	}
+
+	tt := map[string]TestCase{
+		"debug": {
+			level:    pocketlog.LevelDebug,
+			expected: "[DEBUG] this is a debug\n[INFO] this is an info\n[ERROR] this is an erro\n",
+		},
+		"info": {
+			level:    pocketlog.LevelInfo,
+			expected: "[INFO] this is an info\n[ERROR] this is an erro\n",
+		},
+		"error": {
+			level:    pocketlog.LevelError,
+			expected: "[ERROR] this is an erro\n",
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			tw := &testWriter{}
+			testedLogger := pocketlog.New(tc.level, 15, pocketlog.WithOutput(tw))
+
+			testedLogger.Debugf(debugMsg)
+			testedLogger.Logf(pocketlog.LevelInfo, infoMsg)
+			testedLogger.Logf(pocketlog.LevelError, errorMsg)
 			if tw.contents != tc.expected {
 				t.Errorf("invalid contents, expected %q got %q", tc.expected, tw.contents)
 			}
