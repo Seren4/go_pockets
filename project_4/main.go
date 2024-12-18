557d0b9b2605a4d29ffd8631e351dac8feb36743
Seren4 <25780127+Seren4@users.noreply.github.com>
2024-12-18 12:10:26 +0100

    add solution and max attempts

--- project_4/main.go
+++ project_4/main.go
@@ -5,7 +5,10 @@ import (
 	"learngo-pockets/gordle/gordle"
 )
 
+const maxAttempts = 3
+
 func main()  {
-	g := gordle.New(os.Stdin)
+	solution := "pocket"
+	g := gordle.New(os.Stdin, solution, maxAttempts)
 	g.Play()
 }
