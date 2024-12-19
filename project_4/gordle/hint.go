8786fea8c40a03b361dc7427fda15bccadf6852f
Seren4 <25780127+Seren4@users.noreply.github.com>
2024-12-19 18:13:49 +0100

    add feedback

--- project_4/gordle/hint.go
+++ project_4/gordle/hint.go
@@ -1,5 +1,7 @@
 package gordle
 
+import "strings"
+
 // hint describes the validity of a char in a word.
 type hint byte
 
@@ -21,3 +23,15 @@ func (h hint) String() string {
 			return "⚠️"
 		}
 	}
+
+// feedback is a list of hints, one per character  of the word.
+type feedback []hint
+
+// String implements the Stringer interface for a slice of hints.
+func (fb feedback) String() string {
+	sb := strings.Builder{}
+	for _, h := range fb {
+		sb.WriteString(h.String())
+	}
+	return sb.String()
+}
