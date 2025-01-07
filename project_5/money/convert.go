3b061fd6b1378d572ef1eda6d911f62b137b7dad
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-07 14:42:20 +0100

    start project

---
+++ project_5/money/convert.go
@@ -0,0 +1,6 @@
+package money
+
+// Convert applies the change rate to convert an amount to a target currency.
+func Convert(amountToConvert Amount, to Currency) (Amount, error) {
+	return Amount{}, nil
+}
