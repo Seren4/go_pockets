7e382428b07dfda1952c7e90c122ccdd5b87d277
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-07 21:40:37 +0100

    add test helper functions

--- project_5/money/convert_test.go
+++ project_5/money/convert_test.go
@@ -6,6 +6,35 @@ import (
 	"testing"
 )
 
+func mustParseCurrency(t *testing.T, code string) money.Currency {
+	t.Helper()
+	currency, err := money.ParseCurrency(code)
+	if err != nil {
+		// we are not using t.Fail but t.Fatal, which stops the test run immediately.
+		t.Fatalf("cannot parse currency %s code", code)
+	}
+	return currency
+}
+
+func mustParseAmount(t *testing.T, value string, code string) money.Amount {
+	t.Helper()
+	decimal, err := money.ParseDecimal(value)
+	if err != nil {
+		t.Fatalf("invalid number %s", value)
+	}
+	currency, err := money.ParseCurrency(code)
+	if err != nil {
+		t.Fatalf("Invalid currency code %s", code)
+	}
+
+	amount, err := money.NewAmount(decimal, currency)
+	if err != nil {
+		t.Fatalf("cannot create amount with value %v and currency code %s", decimal, code)
+	}
+
+	return amount
+}
+
 func TestConvert(t *testing.T) {
 	tt := map[string]struct {
 		amount money.Amount
@@ -13,8 +42,8 @@ func TestConvert(t *testing.T) {
 		validate func(t *testing.T, got money.Amount, err error)
 	}{
 		"34.98 USD to EUR": {
-			amount: money.Amount{},
-			to:money.Currency{},
+			amount: mustParseAmount(t, "34.98", "USD"),
+			to: mustParseCurrency(t, "EUR"),
 			validate: func(t *testing.T, got money.Amount, err error) {
 				if err != nil {
 					t.Errorf("expected no error, got %s", err.Error())
