31c1dea24516d0205377c0df178fa80dbb674287
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-21 11:41:17 +0100

    test broken xml

--- project_5/ecbank/ecb_internal_test.go
+++ project_5/ecbank/ecb_internal_test.go
@@ -55,6 +55,33 @@ func TestEuroCentralBank_FetchExchangeRate_Error500(t *testing.T) {
 	}
 }
 
+func TestEuroCentralBank_FetchExchangeRate_ErrorXML(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, `<?xml version="1.0" encoding="UTF-8"?><gesmes:Envelope>
+		<gesmes:subject>Reference rates</gesmes:subject>
+		<Cube time='2025-01-17'><Cube>
+		<Cube time='2025-01-17'><Cube>
+		<Cube currency='USD' rate='2'/>
+		<Cube currency='RON' rate='6'/>
+		</Cube></Cube></gesmes:Envelope>`)
+	}))
+	defer ts.Close() 
+
+	ecb := Client{
+		url: ts.URL,
+	}
+	got, err := ecb.FetchExchangeRate(mustParseCurrency(t, "USD"), mustParseCurrency(t,"RON")) 
+	want := mustParseDecimal(t, "0")
+
+	if !errors.Is(err, ErrUnexpectedFormat){
+		t.Errorf("unexpected error: %v, want: %v", err, ErrServerSide)
+	}
+	
+	if want != money.Decimal(got) {
+		t.Errorf("FetchExchangeRate got = %v, want %v", money.Decimal(got), want)
+	}
+}
+
 func mustParseCurrency(t *testing.T, input string) money.Currency {
 	t.Helper()
 	curr, err := money.ParseCurrency(input)
