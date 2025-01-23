35a0e50f529fcb79d5f31f7e1710e32331fd8917
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-23 17:29:34 +0100

    add chronologicalKeys and maxsize; reformat add and dlt ft

--- project_6/cache.go
+++ project_6/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"slices"
 	"sync"
 	"time"
 )
@@ -8,10 +9,12 @@ import (
 // Cache is key-value storage.
 type Cache[K comparable, V any] struct {
 	// add a mutex next to the resource we want to protect (the data map).
-	mu   sync.Mutex
-	// rmu  sync.RWMutex 
-	data map[K]entryWithTimeout[V]
-	ttl  time.Duration
+	mu sync.Mutex
+	// rmu  sync.RWMutex
+	data              map[K]entryWithTimeout[V]
+	ttl               time.Duration
+	maxSize           int
+	chronologicalKeys []K
 }
 
 type entryWithTimeout[V any] struct {
@@ -20,10 +23,12 @@ type entryWithTimeout[V any] struct {
 }
 
 // New creates a usable Cache (it initializes the map for us).
-func New[K comparable, V any](ttl time.Duration) Cache[K, V] {
+func New[K comparable, V any](maxSize int, ttl time.Duration) Cache[K, V] {
 	return Cache[K, V]{
-		ttl:  ttl,
-		data: make(map[K]entryWithTimeout[V]),
+		ttl:               ttl,
+		data:              make(map[K]entryWithTimeout[V]),
+		maxSize:           maxSize,
+		chronologicalKeys: make([]K, 0, maxSize),
 	}
 }
 
@@ -39,7 +44,7 @@ func (c *Cache[K, V]) Read(key K) (V, bool) {
 		return noValue, false
 	}
 	if v.expires.Before(time.Now()) {
-		delete(c.data, key)
+		c.deleteKeyValue(key)
 		return noValue, false
 	}
 	return v.value, true
@@ -49,10 +54,7 @@ func (c *Cache[K, V]) Read(key K) (V, bool) {
 func (c *Cache[K, V]) Upsert(key K, value V) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.data[key] = entryWithTimeout[V]{
-		value:   value,
-		expires: time.Now().Add(c.ttl),
-	}
+	c.addKeyValue(key, value)
 	// Do not return an error for the moment,
 	// but it can happen in the near future.
 	return nil
@@ -62,5 +64,20 @@ func (c *Cache[K, V]) Upsert(key K, value V) error {
 func (c *Cache[K, V]) Delete(key K) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	c.deleteKeyValue(key)
+}
+
+// addKeyValue inserts a key and its value into the cache.
+func (c *Cache[K, V]) addKeyValue(key K, value V) {
+	c.data[key] = entryWithTimeout[V]{
+		value:   value,
+		expires: time.Now().Add(c.ttl),
+	}
+	c.chronologicalKeys = append(c.chronologicalKeys, key)
+}
+
+// deleteKeyValue removes a key and its associated value from the cache.
+func (c *Cache[K, V]) deleteKeyValue(key K) {
 	delete(c.data, key)
+	c.chronologicalKeys = slices.DeleteFunc(c.chronologicalKeys, func(k K) bool { return k == key })
 }
