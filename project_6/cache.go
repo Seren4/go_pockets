c6efd08bc6ab11ed1d457871d10f283fa8d95332
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-23 16:20:19 +0100

    add channels and time to live

--- project_6/cache.go
+++ project_6/cache.go
@@ -1,31 +1,66 @@
 package cache
 
+import (
+	"sync"
+	"time"
+)
+
 // Cache is key-value storage.
 type Cache[K comparable, V any] struct {
-	data  map[K]V
+	// add a mutex next to the resource we want to protect (the data map).
+	mu   sync.Mutex
+	// rmu  sync.RWMutex 
+	data map[K]entryWithTimeout[V]
+	ttl  time.Duration
+}
+
+type entryWithTimeout[V any] struct {
+	value   V
+	expires time.Time // After that time, the value is useless.
 }
 
 // New creates a usable Cache (it initializes the map for us).
-func New[K comparable, V any]() Cache[K, V] {
+func New[K comparable, V any](ttl time.Duration) Cache[K, V] {
 	return Cache[K, V]{
-				 data: make(map[K]V),
-} }
+		ttl:  ttl,
+		data: make(map[K]entryWithTimeout[V]),
+	}
+}
 
-//This method accepts a key of the adequate type, and returns the value - also of the adequate type
-func (c *Cache[K, V]) Read(key K) (V, bool){
+// This method accepts a key of the adequate type, and returns the value - also of the adequate type
+func (c *Cache[K, V]) Read(key K) (V, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var noValue V
+
 	v, found := c.data[key]
-	return v, found
+	if !found {
+		return noValue, false
+	}
+	if v.expires.Before(time.Now()) {
+		delete(c.data, key)
+		return noValue, false
+	}
+	return v.value, true
 }
 
 // Upsert overrides the value for a given key.
 func (c *Cache[K, V]) Upsert(key K, value V) error {
-	c.data[key] = value
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data[key] = entryWithTimeout[V]{
+		value:   value,
+		expires: time.Now().Add(c.ttl),
+	}
 	// Do not return an error for the moment,
-  // but it can happen in the near future.
-  return nil
+	// but it can happen in the near future.
+	return nil
 }
 
 // Delete removes the entry for the given key.
 func (c *Cache[K, V]) Delete(key K) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	delete(c.data, key)
 }
--- project_6/cache_test.go
+++ project_6/cache_test.go
@@ -1,20 +1,29 @@
 package cache_test
 
 import (
-	"testing"
+	"fmt"
 	cache "learngo-pockets/genericcache"
-	"github.com/stretchr/testify/assert"
 	"reflect"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCache(t *testing.T) {
-	c := cache.New[string, string]()
-	
+	c := cache.New[string, string](time.Millisecond * 100)
+
 	c.Upsert("serena", "06778899")
 	value, found := c.Read("serena")
 	assert.True(t, found)
 	assert.Equal(t, "06778899", value)
 	assert.Equal(t, reflect.TypeOf(value).String(), "string")
+
+	time.Sleep(time.Millisecond * 200)
+	v, f := c.Read("serena")
+	assert.False(t, f)
+	assert.Equal(t, "", v)
 
 	value2, found2 := c.Read("alice")
 	assert.False(t, found2)
@@ -25,4 +34,55 @@ func TestCache(t *testing.T) {
 	assert.True(t, found3)
 	assert.Equal(t, "06778800", value3)
 
+}
+
+func TestCache_Parallel_goroutines(t *testing.T) {
+
+	c := cache.New[int, string](30)
+
+	const parallelTasks = 10
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(parallelTasks)
+
+	for i := 0; i < parallelTasks; i++ {
+		go func(j int) {
+			defer wg.Done()
+			c.Upsert(4, fmt.Sprint(j))
+		}(i)
+	}
+	wg.Wait()
+}
+
+// Alternatively, we can make use of the testing package to execute parallel tests.
+func TestCache_Parallel(t *testing.T) {
+
+	c := cache.New[int, string](30)
+
+	// This goroutine can be executed along with another.
+	t.Run("write six", func(t *testing.T) {
+		t.Parallel()
+		c.Upsert(6, "six")
+	})
+
+	// This goroutine can be executed along with another.
+	t.Run("write kuus", func(t *testing.T) {
+		t.Parallel()
+		c.Upsert(6, "kuus")
+	})
+}
+
+func TestCache_TTL(t *testing.T) {
+	t.Parallel()
+	c := cache.New[string, string](time.Millisecond * 100)
+	c.Upsert("Norwegian", "Blue")
+	// Check the item is there.
+	got, found := c.Read("Norwegian")
+	assert.True(t, found)
+	assert.Equal(t, "Blue", got)
+	time.Sleep(time.Millisecond * 200)
+	got, found = c.Read("Norwegian")
+	assert.False(t, found)
+	assert.Equal(t, "", got)
 }
