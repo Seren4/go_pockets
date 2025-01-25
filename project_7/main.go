b93287e22b79f3685a4526358984a81228a370e3
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-25 23:13:57 +0100

    dont' panic

--- project_7/main.go
+++ project_7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -10,8 +11,7 @@ func main() {
 	// func ListenAndServe(addr string, handler Handler) error
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		// It’s OK to panic in the body of main.
-		panic(err)                                                   
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)                                             
 	}
-	fmt.Println(err)
 }
