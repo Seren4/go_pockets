b5320304a0e2bcafb2129c48eeee2eb87386325e
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-26 14:45:21 +0100

    only accept POST req

--- project_7/internal/handlers/newgame/handler.go
+++ project_7/internal/handlers/newgame/handler.go
@@ -11,5 +11,10 @@ import "net/http"
 // func (mux *ServeMux) HandleFunc(pattern string, handler func(ResponseWriter, *Request))
 
 func Handle(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 	_, _ = w.Write([]byte("Creating a new game"))
 }
