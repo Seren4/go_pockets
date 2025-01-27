d7ea7b41a67adee265fc562f14b8e79a51963eb1
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-27 13:28:13 +0100

    mv file

---
+++ project_7/internal/handlers/newgame/handler_test.go
@@ -0,0 +1,28 @@
+package newgame_test
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"learngo/httpgordle/internal/handlers/newgame"
+
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+
+)
+
+
+func TestHandle(t *testing.T) {
+	// Create a request.
+	req, err := http.NewRequest(http.MethodPost, "/games", nil)  
+	require.NoError(t, err)
+	// Create a response recorder
+	recorder := httptest.NewRecorder() 
+
+	// Call the function
+	newgame.Handle(recorder, req)  
+	assert.Equal(t, http.StatusCreated, recorder.Code)
+	assert.Equal(t,"application/json", recorder.Header().Get("Content-Type"))
+	assert.JSONEq(t, `{"id":"","attempts_left":0,"guesses":null,"word_length":0,"status":""}`, recorder.Body.String())
+}
