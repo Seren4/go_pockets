49a36fa5b8c87fe6bb0aac63b7d951d452e338c9
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-01-31 15:27:57 +0100

    mutex

--- project_7/internal/repository/memory.go
+++ project_7/internal/repository/memory.go
@@ -3,10 +3,12 @@ package repository
 import (
 	"fmt"
 	"learngo/httpgordle/internal/session"
+	"sync"
 )
 
 // GameRepository holds all the current games.
 type GameRepository struct {
+	mutex   sync.Mutex
 	storage map[session.GameID]session.Game
 }
 
@@ -18,6 +20,9 @@ func New() *GameRepository {
 
 // Add inserts for the first time a game in memory.
 func (gr *GameRepository) Add(game session.Game) error {
+	// Lock the reading and the writing of the game.
+	gr.mutex.Lock()
+	defer gr.mutex.Unlock()
 	_, found := gr.storage[game.ID]
 	if found {
 		// Cannot add the same game twice
@@ -30,6 +35,9 @@ func (gr *GameRepository) Add(game session.Game) error {
 
 // Find finds the game in memory (if any).
 func (gr *GameRepository) Find(gameID session.GameID) (session.Game, error) {
+	// Lock the reading of the game.
+	gr.mutex.Lock()
+	defer gr.mutex.Unlock()
 	game, found := gr.storage[gameID]
 	if !found {
 		// Cannot find the game.
@@ -45,6 +53,10 @@ func (gr *GameRepository) Update(game session.Game) error {
 	if err != nil {
 		return fmt.Errorf("error: %s", err)
 	}
+	// Lock the writing of the game.
+
+	gr.mutex.Lock()
+	defer gr.mutex.Unlock()
 	gr.storage[game.ID] = game
 
 	return nil
