124ba7ddef17daf4b1467d55a0bbc81fa8280518
Seren4 <25780127+Seren4@users.noreply.github.com>
2025-02-02 16:38:19 +0100

    guess + tests

--- project_7/internal/handlers/guess/handler.go
+++ project_7/internal/handlers/guess/handler.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"learngo/httpgordle/internal/api"
 	"learngo/httpgordle/internal/session"
+	"learngo/httpgordle/internal/gordle"
+
 	"log"
 	"net/http"
+	"fmt"
+	"errors"
 )
 
 // The benefit of this method over http.Handle is that we don’t have to
@@ -43,12 +47,24 @@ func Handle(db gameGuesser) http.HandlerFunc {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		game, error := guess(id, r)
+		game, error := guess(session.GameID(id), r.Guess, db)
 		if error != nil {
-			// The header has already been set. Nothing much we can do here.
-			log.Printf("failed to write response: %s", err)
-		}
+			switch {
+				case errors.Is(err, session.ErrNotFound):
+					http.Error(w, err.Error(), http.StatusNotFound)
+				case errors.Is(err, gordle.ErrInvalidWordlLength):
+					http.Error(w, err.Error(), http.StatusBadRequest)
+				case errors.Is(err, session.ErrGameOver):
+					http.Error(w, err.Error(), http.StatusForbidden)
+				default:
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+				return
+			}
+	
 		apiGame := api.ToGameResponse(game)
+		w.Header().Set("Content-Type", "application/json")
+
 		// Encode the game into JSON
 		errorApiGame := json.NewEncoder(w).Encode(apiGame)
 		if errorApiGame != nil {
@@ -59,8 +75,42 @@ func Handle(db gameGuesser) http.HandlerFunc {
 	}
 }
 
-func guess(id string, r api.GuessRequest) (session.Game, error) {
-	return session.Game{
-		ID: session.GameID(id),
-	}, nil
+func guess(id session.GameID, guess string, db gameGuesser) (session.Game, error) {
+	game, err := db.Find(id)  
+	if game.AttemptsLeft == 0 || game.Status == session.StatusWon { 
+		return session.Game{}, session.ErrGameOver
+	}
+	if err != nil {
+		log.Printf("failed to find the game %s: %v", id, err)
+		return session.Game{}, fmt.Errorf("can't find the game: %w", err)
+	}
+
+	fb, err := game.Gordle.Play(guess)
+
+	if err != nil {
+		return session.Game{}, fmt.Errorf("unable to play: %w", err)
+	}
+
+	game.Guesses = append(game.Guesses, session.Guess{
+		Word:     guess,
+		Feedback: fb.String(),
+	})
+
+	game.AttemptsLeft -= 1
+
+	switch {                                                      
+		case fb.GameWon():
+			game.Status = session.StatusWon
+		case game.AttemptsLeft == 0:
+			game.Status = session.StatusLost
+		default:
+			game.Status = session.StatusPlaying
+		}
+
+	err = db.Update(game) 
+	if err != nil {
+		return session.Game{}, fmt.Errorf("unable to save game: %w", err)
+	}
+	return game, nil
+
 }
--- project_7/internal/handlers/guess/handler_test.go
+++ project_7/internal/handlers/guess/handler_test.go
@@ -4,6 +4,8 @@ import (
 	"learngo/httpgordle/internal/api"
 	"learngo/httpgordle/internal/handlers/guess"
 	"learngo/httpgordle/internal/session"
+	"learngo/httpgordle/internal/gordle"
+
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -14,6 +16,17 @@ import (
 )
 
 func TestHandleGet(t *testing.T) {
+
+	game, _ := gordle.New([]string { "pocket" })
+
+	handleFunc := guess.Handle(gameGuesserStub{session.Game{
+		ID:           "34",
+		Gordle:       *game,
+		AttemptsLeft: 3,
+		Guesses:      nil,
+		Status:       session.StatusPlaying,
+	}})
+
 
 	// Create a request with a non nil body; (body is an io.Reader)
 	body := strings.NewReader(`{"guess":"pocket"}`)
@@ -26,26 +39,24 @@ func TestHandleGet(t *testing.T) {
 	// Create a response recorder
 	recorder := httptest.NewRecorder()
 
-	handleFunc := guess.Handle(gameGuesserStub{})
 	// Call the function
 
 	handleFunc(recorder, req)
 	assert.Equal(t, http.StatusOK, recorder.Code)
 
-	//assert.Equal(t,"application/json", recorder.Header().Get("Content-Type"))
-	assert.JSONEq(t, `{"id":"34","attempts_left":0,"guesses":[],"word_length":0,"status":""}`, recorder.Body.String())
+	assert.Equal(t,"application/json", recorder.Header().Get("Content-Type"))
+	assert.JSONEq(t, `{"id":"34","attempts_left":2,"guesses":[{"word":"pocket", "feedback":"✅✅✅✅✅✅"}],"word_length":0,"status":"Won"}`, recorder.Body.String())
 }
 
 // Stubbing the repo
 type gameGuesserStub struct {
 	game session.Game
-	err  error
 }
 
 func (g gameGuesserStub) Update(_ session.Game) error {
-	return g.err
+	return nil
 }
 
 func (g gameGuesserStub) Find(_ session.GameID) (session.Game, error) {
-	return g.game, g.err
+	return g.game, nil
 }
